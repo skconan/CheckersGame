4d156a19d938a928f146de2b0f008596cba5c742
skconan <tulconan@gmail.com>
2016-12-05 19:15:30 +0700

    add ModelSprite

--- checker.py
+++ checker.py
@@ -1,5 +1,5 @@
 import arcade
-from models import Control
+from models import World, Control, Map
 
 
 SCREEN_WIDTH = 1024
@@ -11,26 +11,43 @@ src = {"bg": "images/board.fw.png",
        "select": "images/select.fw.png"}
 
 
+class ModelSprite(arcade.Sprite):
+
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+                self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
+
 class CheckerGameWindow(arcade.Window):
 
     def __init__(self, width, height):
         super().__init__(width, height)
         self.bg = arcade.Sprite(src['bg'])
         self.bg.set_position(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        self.control = Control(BOTTOM_LEFT[0], BOTTOM_LEFT[1])
-        self.control_sprite = arcade.Sprite(src['control'])
-        # self.
-
+        self.world = World(BOTTOM_LEFT)
+        self.map = self.world.map
+        self.control_sprite = ModelSprite(
+            src['control'], model=self.world.control)
+       
     def on_draw(self):
         arcade.start_render()
         self.bg.draw()
         self.control_sprite.draw()
-                
+        self.map.draw_player()
+    
     def animate(self, delta):
-        control = self.control
-        control.animate()
-        self.control_sprite.set_position(control.r, control.c)
+        self.map.animate(delta)
 
+    def on_key_release(self, key, key_modifiers):
+        self.world.on_key_release(key, key_modifiers)
 
 if __name__ == '__main__':
     window = CheckerGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
