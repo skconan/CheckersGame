2286d0d160b453ea0b810f0655dfde3b1a27e515
skconan <tulconan@gmail.com>
2016-12-09 12:03:22 +0700

    delete  ModelSprite

--- checker.py
+++ checker.py
@@ -1,36 +1,17 @@
 import arcade
-from models import World, Control, Map, WorldRenderer
+from models import World, WorldRenderer
 
 
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 700
-BOTTOM_LEFT = (237, 73)
-
-src = {"bg": "images/board.fw.png",
-       "control": "images/control.fw.png",
-       "select": "images/select.fw.png"}
-
-
-class ModelSprite(arcade.Sprite):
-
-    def __init__(self, *args, **kwargs):
-        self.model = kwargs.pop('model', None)
-        super().__init__(*args, **kwargs)
-
-    def sync_with_model(self):
-        if self.model:
-            self.set_position(self.model.x, self.model.y)
-
-    def draw(self):
-        self.sync_with_model()
-        super().draw()
 
 
 class CheckerGameWindow(arcade.Window):
 
     def __init__(self, width, height):
         super().__init__(width, height)
-        self.bg = arcade.Sprite(src['bg'])
+        self.src = {"bg": "images/board.fw.png"}
+        self.bg = arcade.Sprite(self.src['bg'])
         self.bg.set_position(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         self.world = World()
         self.worldRenderer = WorldRenderer(self.world)
@@ -39,7 +20,7 @@ class CheckerGameWindow(arcade.Window):
         arcade.start_render()
         self.bg.draw()
         self.worldRenderer.on_draw()
- 
+
     def animate(self, delta):
         self.world.animate(delta)
 
