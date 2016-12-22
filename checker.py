17b8b00315544cd674ef01970cf0935592846ada
skconan <tulconan@gmail.com>
2016-12-22 10:02:58 +0700

    change screen_width and change bg  to texture

--- checker.py
+++ checker.py
@@ -1,8 +1,7 @@
 import arcade
 from models import World, WorldRenderer
 
-
-SCREEN_WIDTH = 1024
+SCREEN_WIDTH = 1098
 SCREEN_HEIGHT = 700
 
 
@@ -11,14 +10,15 @@ class CheckerGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.src = {"bg": "images/board.fw.png"}
-        self.bg = arcade.Sprite(self.src['bg'])
-        self.bg.set_position(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        self.bg = arcade.load_texture(self.src['bg'])
         self.world = World()
         self.worldRenderer = WorldRenderer(self.world)
 
     def on_draw(self):
         arcade.start_render()
-        self.bg.draw()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                      self.bg.width,
+                                      self.bg.height, self.bg, 0)
         self.worldRenderer.on_draw()
 
     def animate(self, delta):
