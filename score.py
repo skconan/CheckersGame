75cca176daddc28458db7fa62c1387f939477b6e
skconan <tulconan@gmail.com>
2016-12-23 01:11:52 +0700

    delete line

--- score.py
+++ score.py
@@ -1,30 +1,35 @@
 import arcade
 import constants as const
+
+
 class Score:
 
     def __init__(self):
-        self.score_bot, self.score_player = 0,0
+        self.score_bot, self.score_player = 0, 0
         self.src_player = "images/p0.fw.png"
         self.src_bot = "images/p0.fw.png"
         self.update_img()
 
     def update_img(self):
-        self.src_player = "images/p"+str(self.score_player)+".fw.png"
-        self.src_bot = "images/b"+str(self.score_bot)+".fw.png"  
+        self.src_player = "images/p" + str(self.score_player) + ".fw.png"
+        self.src_bot = "images/b" + str(self.score_bot) + ".fw.png"
         self.player_text = arcade.Sprite(self.src_player)
         self.bot_text = arcade.Sprite(self.src_bot)
-        self.player_text.set_position(const.SCREEN_WIDTH - 120 ,const.SCREEN_HEIGHT/2 + 100)
-        self.bot_text.set_position(120,const.SCREEN_HEIGHT/2 + 100)
+        self.player_text.set_position(
+            const.SCREEN_WIDTH - 120, const.SCREEN_HEIGHT / 2 + 100)
+        self.bot_text.set_position(120, const.SCREEN_HEIGHT / 2 + 100)
 
     def increase(self, character):
         if character == 'p':
             self.score_player += 1
         else:
             self.score_bot += 1
+
         if self.score_player == 8:
             const.GAME_STATE = 2
-        if self.score_bot == 8:
+        elif self.score_bot == 8:
             const.GAME_STATE = 3
+
         self.update_img()
 
     def on_draw(self):
