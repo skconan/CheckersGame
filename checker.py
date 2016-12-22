121d8d1c6a4a7422aca67b7d1de1cb6199853bac
skconan <tulconan@gmail.com>
2016-12-22 15:04:00 +0700

    created menu game

--- checker.py
+++ checker.py
@@ -7,25 +7,31 @@ class CheckerGameWindow(arcade.Window):
 
     def __init__(self, width, height):
         super().__init__(width, height)
-        self.src = {"start": "images/start.fw.png",
-                    "bg": "images/board.fw.png"}
-        self.bg = arcade.load_texture(self.src['bg'])
-        self.world = World()
-        self.worldRenderer = WorldRenderer(self.world)
+        self.src = ["images/start.fw.png","images/board.fw.png"]
+        self.texture = []
+        for src in self.src:
+            self.texture.append(arcade.load_texture(src))
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(const.SCREEN_WIDTH // 2, const.SCREEN_HEIGHT // 2,
-                                      self.bg.width,
-                                      self.bg.height, self.bg, 0)
-        self.worldRenderer.on_draw()
+                                      self.texture[const.GAME_STATE].width,
+                                      self.texture[const.GAME_STATE].height, self.texture[const.GAME_STATE], 0)
+        if const.GAME_STATE == 1:
+            self.worldRenderer.on_draw()
 
     def animate(self, delta):
-        self.world.animate(delta)
+        if const.GAME_STATE == 1:
+            self.world.animate(delta)
 
     def on_mouse_release(self, x, y, button, modifiers):
-        self.world.on_mouse_release(x, y, button, modifiers)
-
+        if const.GAME_STATE == 0:
+            self.world = World()
+            self.worldRenderer = WorldRenderer(self.world)
+            const.GAME_STATE = 1
+        else :
+            self.world.on_mouse_release(x, y, button, modifiers)
+        
 if __name__ == '__main__':
     window = CheckerGameWindow(const.SCREEN_WIDTH, const.SCREEN_HEIGHT)
     arcade.run()
